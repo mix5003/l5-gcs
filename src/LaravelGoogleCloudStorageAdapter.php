76105c78c4ec36207d34973fb4e606014054c69b
mix5003 <mix5003@gmail.com>
2017-11-25 12:10:57 +0700

    can use custom url

--- src/LaravelGoogleCloudStorageAdapter.php
+++ src/LaravelGoogleCloudStorageAdapter.php
@@ -5,6 +5,8 @@ namespace mix5003\GCSProvider;
 
 
 use CedricZiel\FlysystemGcs\GoogleCloudStorageAdapter;
+use League\Flysystem\AdapterInterface;
+use League\Flysystem\Config;
 
 class LaravelGoogleCloudStorageAdapter extends GoogleCloudStorageAdapter
 {
@@ -12,18 +14,29 @@ class LaravelGoogleCloudStorageAdapter extends GoogleCloudStorageAdapter
     /**
      * Get a temporary URL for the file at the given path.
      *
-     * @param  string  $path
-     * @param  \DateTimeInterface  $expiration
-     * @param  array  $options
+     * @param  string $path
+     * @param  \DateTimeInterface $expiration
+     * @param  array $options
      * @return string
      */
     public function getTemporaryUrl($path, $expiration, $options = [])
     {
         $path = $this->applyPathPrefix($path);
 
-        return $this->bucket
+        if (empty($options['cname']) && $this->isCustomUrl()) {
+            $options['cname'] = $this->baseUrl;
+        }
+
+        $signedUrl = $this->bucket
             ->object($path)
             ->signedUrl($expiration, $options);
+
+        if (!empty($options['cname'])) {
+            $scheme = parse_url($this->baseUrl, PHP_URL_SCHEME);
+            $signedUrl = $scheme . '://' . substr($signedUrl, strlen($this->baseUrl));
+        }
+
+        return $signedUrl;
     }
 
     /**
@@ -42,5 +55,15 @@ class LaravelGoogleCloudStorageAdapter extends GoogleCloudStorageAdapter
         }
 
         return $options;
+    }
+
+    /**
+     * Check if it custom url
+     *
+     * @return boolean
+     */
+    private function isCustomUrl()
+    {
+        return substr($this->baseUrl, 0, strlen(self::GCS_BASE_URL)) !== self::GCS_BASE_URL;
     }
 }
